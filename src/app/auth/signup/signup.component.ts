60ef2fa8e1aa20ba852677e4b1e71cd482ad64f3
Tenesh <teneshdev@gmail.com>
2022-05-02 04:12:43 +0800

    CHAT-11 Update signup form and add password validator.
    Add signup form group and custom validator for match passwords.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+import {passwordValidator} from './password.validator';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
   title = 'chatty';
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required, Validators.minLength(6),Validators.pattern('^[a-zA-Z]{6,18}$')]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,32}$')]),
+      'confirm-password': new FormControl(null, [Validators.required])
+    }, {
+      validators: passwordValidator('password', 'confirm-password')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm)
+  }
 }
