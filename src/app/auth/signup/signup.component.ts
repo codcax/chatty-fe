682513eaf6f7f7884ba832764d55c9e4bff4f39a
Tenesh <teneshdev@gmail.com>
2022-05-02 10:08:38 +0800

    CHAT-11 Update signup form error and add color variables
    Add error class for inputs on validation and add input helper div when form control on focus.
    Add color variables for error, warning and success.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -11,10 +11,11 @@ import {passwordValidator} from './password.validator';
 export class SignUpComponent implements OnInit {
   title = 'chatty';
   signupForm: FormGroup;
+  focusControl: string | undefined;
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, [Validators.required, Validators.minLength(6),Validators.pattern('^[a-zA-Z]{6,18}$')]),
+      'username': new FormControl(null, [Validators.required, Validators.minLength(6), Validators.pattern('^[a-zA-Z]{6,18}$')]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,32}$')]),
       'confirm-password': new FormControl(null, [Validators.required])
@@ -25,5 +26,15 @@ export class SignUpComponent implements OnInit {
 
   onSubmit() {
     console.log(this.signupForm)
+  }
+
+  onFocus(event: any) {
+    if (event.target.attributes.formControlName.value === 'password') {
+      this.focusControl = 'password'
+    } else if (event.target.attributes.formControlName.value === 'username') {
+      this.focusControl = 'username'
+    } else {
+      this.focusControl = undefined
+    }
   }
 }
